d3e592f46154702d1eb52ab15198318fd4f60345
Saeide Amini <37140636+samini1989@users.noreply.github.com>
2024-09-05 04:10:46 -0700

    Add files via upload

---
+++ testtime.py
@@ -0,0 +1,12 @@
+import datetime
+#from hijridate import Hijri
+
+##year=int(input('سال تولد:'))
+##month=int(input('ماه تولد:'))
+##day=int(input('روز تولد:'))
+##
+##b=Hijri(year,month,day)
+##print(b)
+
+age=int(input('Age:')) 
+print('سال تولد:', 1403-age)
